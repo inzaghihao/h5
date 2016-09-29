da0a0034b6e3726e655002e97eaf20c107fb70b4
inzaghihao <wangyipippo@163.com>
2016-09-29 09:22:59 +0800

    verson1

---
+++ js/h5componentbase.js
@@ -0,0 +1,28 @@
+
+var H5componentbase = function(name,cfg){
+	var cfg = cfg || {};
+	
+	var id = ( 'h5_c_'+Math.random() ).replace(".","_");
+	var cls = "h5_component_" + cfg.type + " h5_component_name" + name;
+
+	var component = $("<div class='h5component "+cls+"' id='"+id+"'></div>");
+	cfg.text && component.text(cfg.text);
+	cfg.width && component.width(cfg.width/2);
+	cfg.height && component.height(cfg.height/2);
+
+	cfg.css && component.css(cfg.css);
+	cfg.bg && component.css("backgroundImage","url("+cfg.bg+")");
+	
+	if(cfg.center === true){
+		component.css({
+			left:"50%",
+			marginLeft : (cfg.width/4*-1) + "px"
+		})
+	}
+	return component;
+}
+
+
+
+
+
